620d76acd0c98fa6e6686deab684c473dfe92213
ydworkLab <service@youdo.com>
2025-07-08 12:08:28 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "VK-iOS-SDK",
+            path: "library",
             resources: [
                 .copy("VkResources"),
             ],
