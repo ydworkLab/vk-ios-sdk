be56a72b8ea3cd1aa4d6184291b164f99658ca74
ydworkLab <service@youdo.com>
2025-07-09 12:36:27 +0300

    Update VKUploadWallPhotoRequest.h

--- library/Source/API/upload/VKUploadWallPhotoRequest.h
+++ library/Source/API/upload/VKUploadWallPhotoRequest.h
@@ -20,7 +20,7 @@
 //  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <VKSdkFramework/VKUploadPhotoBase.h>
+#import "VKUploadPhotoBase.h"
 
 /**
 Special request for upload single photo to user wall
