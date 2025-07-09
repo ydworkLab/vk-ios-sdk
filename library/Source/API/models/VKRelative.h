103fc53c7fac4dfe877e99ecc55314143e85bd8f
ydworkLab <service@youdo.com>
2025-07-09 12:29:38 +0300

    Update VKRelative.h

--- library/Source/API/models/VKRelative.h
+++ library/Source/API/models/VKRelative.h
@@ -20,7 +20,7 @@
 //  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <VKSdkFramework/VKApiObjectArray.h>
+#import "VKApiObjectArray.h"
 
 @interface VKRelative : VKApiObject
 
