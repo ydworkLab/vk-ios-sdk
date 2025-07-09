344de889ef10e636038161bfd551478bb40aafc7
ydworkLab <service@youdo.com>
2025-07-09 12:28:49 +0300

    Update VKLikes.h

--- library/Source/API/models/VKLikes.h
+++ library/Source/API/models/VKLikes.h
@@ -21,7 +21,7 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
-#import <VKSdkFramework/VKApiObject.h>
+#import "VKApiObject.h"
 
 /**
 Likes type of VK API
