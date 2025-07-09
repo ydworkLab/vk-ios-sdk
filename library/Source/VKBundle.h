6bfde709a03a1eed371fe64d284d5ad545ef5060
ydworkLab <service@youdo.com>
2025-07-09 12:45:03 +0300

    Update VKBundle.h

--- library/Source/VKBundle.h
+++ library/Source/VKBundle.h
@@ -20,7 +20,7 @@
 //  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <VKSdkFramework/VKObject.h>
+#import "Core/VKObject.h"
 
 #define VKLocalizedString(s) [VKBundle localizedString:s]
 #define VKImageNamed(s)      [VKBundle vkLibraryImageNamed:s]
