7fb2068d011a7634f507750b766600b2b23eef8c
ydworkLab <service@youdo.com>
2025-07-09 12:31:24 +0300

    Update VKApiCaptcha.h

--- library/Source/API/methods/VKApiCaptcha.h
+++ library/Source/API/methods/VKApiCaptcha.h
@@ -20,7 +20,7 @@
 //  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <VKSdkFramework/VKApiBase.h>
+#import "VKApiBase.h"
 
 /**
 Methods for captcha work. Currently, it is 1 method for forcing captcha response
