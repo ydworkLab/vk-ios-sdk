de21aa59ea15377975bd3b6f349d111ef0ac2d26
ydworkLab <service@youdo.com>
2025-07-09 12:25:56 +0300

    Update VKApiObject.h

--- library/Source/API/models/VKApiObject.h
+++ library/Source/API/models/VKApiObject.h
@@ -21,7 +21,7 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <objc/runtime.h>
-#import <VKSdkFramework/VKObject.h>
+#import "../../Core/VKObject.h"
 
 /**
 Helps in objects parsing
